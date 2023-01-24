f763930dfa569ef058326d110e20dc376cc434c5
mmdxhg <mmdxhg@163.com>
2023-01-24 17:51:11 +0800

    time: 2022_01_18 info:first commit for flinkReLearn work: broadcast Stream and maxwell_mysqlDS for dim

--- src/main/java/com/atguigu/gmall/realtime/util/KafkaUtil.java
+++ src/main/java/com/atguigu/gmall/realtime/util/KafkaUtil.java
@@ -2,25 +2,32 @@ package com.atguigu.gmall.realtime.util;
 
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import javax.annotation.Nullable;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class KafkaUtil {
 
-    public static String BOOTSTRAP_SERVER = "hadoop102:9092,hadoop103:9092,hadoop104:9092";
+    public static String BOOTSTRAP_SERVERS = "hadoop102:9092,hadoop103:9092,hadoop104:9092";
 
-    public static FlinkKafkaConsumer<String>  getKafkaConsumer(String topic,String groupId){
+    public static String DEFAULT_TOPIC = "default_topic";
 
+
+    public static FlinkKafkaConsumer<String> getFlinkKafkaConsumer(String topic,String groupId){
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers",BOOTSTRAP_SERVER);
-        properties.setProperty("topic",topic);
+        properties.setProperty("bootstrap.servers",BOOTSTRAP_SERVERS);
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
 
-        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(topic, new KafkaDeserializationSchema<String>() {
+        FlinkKafkaConsumer<String> stringFlinkKafkaConsumer = new FlinkKafkaConsumer<String>(
+                topic, new KafkaDeserializationSchema<String>() {
             @Override
             public boolean isEndOfStream(String nextElement) {
                 return false;
@@ -30,7 +37,7 @@ public class KafkaUtil {
             public String deserialize(ConsumerRecord<byte[], byte[]> record) throws Exception {
 
                 if (record != null && record.value() != null){
-                    return new String(record.value(), StandardCharsets.UTF_8);
+                    return new String(record.value(),StandardCharsets.UTF_8);
                 }
                 return null;
             }
@@ -39,7 +46,29 @@ public class KafkaUtil {
             public TypeInformation<String> getProducedType() {
                 return TypeInformation.of(String.class);
             }
-        }, properties);
-        return kafkaConsumer;
+        },properties
+        );
+
+        return stringFlinkKafkaConsumer;
+    }
+
+    public static FlinkKafkaProducer<String> getKafkaProducer(String topic){
+        Properties properties = new Properties();
+        properties.setProperty(
+                "bootstrap.servers",BOOTSTRAP_SERVERS
+        );
+        properties.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG,
+                60 * 15 * 1000 + "");
+
+        FlinkKafkaProducer<String> stringFlinkKafkaProducer = new FlinkKafkaProducer<String>(
+                DEFAULT_TOPIC, new KafkaSerializationSchema<String>() {
+            @Override
+            public ProducerRecord<byte[], byte[]> serialize(String element, @Nullable Long timestamp) {
+                return new ProducerRecord<byte[], byte[]>(topic,element.getBytes());
+            }
+        },properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+        );
+
+        return stringFlinkKafkaProducer;
     }
 }
