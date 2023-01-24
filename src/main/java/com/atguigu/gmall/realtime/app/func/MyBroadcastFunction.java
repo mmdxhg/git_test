f731c47505ecaeaa72abb4b90588bc5653e26a53
mmdxhg <mmdxhg@163.com>
2023-01-24 17:50:31 +0800

    time: 2022_01_18 info:first commit for flinkReLearn work: broadcast Stream and maxwell_mysqlDS for dim

--- src/main/java/com/atguigu/gmall/realtime/app/func/MyBroadcastFunction.java
+++ src/main/java/com/atguigu/gmall/realtime/app/func/MyBroadcastFunction.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.atguigu.gmall.realtime.bean.TableProcess;
 import com.atguigu.gmall.realtime.common.GmallConfig;
 import com.mysql.cj.xdevapi.Table;
+import javafx.scene.control.Tab;
 import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
@@ -19,115 +20,108 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
-public class MyBroadcastFunction extends BroadcastProcessFunction<JSONObject,String, JSONObject> {
+public class MyBroadcastFunction extends BroadcastProcessFunction<JSONObject, String, JSONObject> {
 
-    private MapStateDescriptor<String, TableProcess> mapStateDescriptor;
+    private  MapStateDescriptor<String, TableProcess> mapStateDescriptor;
 
-    public MyBroadcastFunction(MapStateDescriptor<String,TableProcess> mapState) {
-        this.mapStateDescriptor = mapState;
-    }
-
-    //为了能够在广播流进入到程序的时候 ， 就创建出对应的phoenix 的 数据表
-    private Connection connection;
-
+    private static Connection connection;
     @Override
     public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
         Class.forName(GmallConfig.PHOENIX_DRIVER);
         connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+    }
+
+    public MyBroadcastFunction(MapStateDescriptor<String, TableProcess> mapStateDescriptor) {
+        this.mapStateDescriptor = mapStateDescriptor;
     }
 
     @Override
     public void processElement(JSONObject value, ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
         ReadOnlyBroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(mapStateDescriptor);
 
-        String sourceTableName = value.getString("table");
-        TableProcess tableProcess = broadcastState.get(sourceTableName);
+        String table = value.getString("table");
+        TableProcess tableProcess = broadcastState.get(table);
 
         if (tableProcess != null){
             JSONObject data = value.getJSONObject("data");
-            String sinkTable = tableProcess.getSinkTable();
 
+            String sinkTable = tableProcess.getSinkTable();
             String sinkColumns = tableProcess.getSinkColumns();
-            filter(data,sinkColumns);
+            filterColumns(data,sinkColumns);
 
             data.put("sinkTable",sinkTable);
-
             out.collect(data);
         }
     }
 
-    private void filter(JSONObject data, String sinkColumns) {
-        data.entrySet().removeIf(d -> !sinkColumns.contains(d.getKey()));
+    private void filterColumns(JSONObject data, String sinkColumns) {
+        Set<Map.Entry<String, Object>> entries = data.entrySet();
+        entries.removeIf( d -> !sinkColumns.contains(d.getKey()));
     }
 
     @Override
     public void processBroadcastElement(String value, Context ctx, Collector<JSONObject> out) throws Exception {
         BroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(mapStateDescriptor);
 
-        TableProcess config = JSON.parseObject(value).getObject("after", TableProcess.class);
+        TableProcess tableProcess = JSON.parseObject(value).getObject("after", TableProcess.class);
+        String sourceTable = tableProcess.getSourceTable();
+        String sinkTable = tableProcess.getSinkTable();
+        String sinkColumns = tableProcess.getSinkColumns();
+        String sinkExtend = tableProcess.getSinkExtend();
+        String sinkPk = tableProcess.getSinkPk();
 
-
-        String sinkColumns = config.getSinkColumns();
-        String sinkExtend = config.getSinkExtend();
-        String sourceTable = config.getSourceTable();
-        String sinkTable = config.getSinkTable();
-        String sinkPk = config.getSinkPk();
-
-        broadcastState.put(sourceTable,config);
+        broadcastState.put(sourceTable,tableProcess);
 
         checkTable(sinkTable,sinkColumns,sinkPk,sinkExtend);
     }
 
     private void checkTable(String sinkTable, String sinkColumns, String sinkPk, String sinkExtend) {
-        /* 建表语句 ：
-        CREATE TABLE IF NOT EXISTS GMALL2022_REALTIME_TEST.test
-        (
-        	id varchar PRIMARY KEY,
-        	name varchar
-        )
-        */
+
+        //在这里要完成建表语句的封装工作
         StringBuilder sql = new StringBuilder();
+        sql.append("create table if not exists " + GmallConfig.HBASE_SCHEMA + "." + sinkTable + "(\n" );
 
-        sql.append("create table if not exists " + GmallConfig.HBASE_SCHEMA + "." + sinkTable+ "(\n");
+        String[] columns = sinkColumns.split(",");
 
-        String[] split = sinkColumns.split(",");
         if (sinkPk == null){
             sinkPk = "id";
         }
         if (sinkExtend == null){
             sinkExtend = "";
         }
-        for (int i = 0; i < split.length; i++) {
-            sql.append(split[i] + " varchar");
-            if (split[i].equals(sinkPk)){
+        for (int i = 0; i < columns.length; i++) {
+            sql.append(columns[i] + " varchar ");
+            if (sinkPk.equals(columns[i])){
                 sql.append(" primary key");
             }
-            if (i < split.length - 1){
-                sql.append(" ,\n");
+
+            if (i < columns.length - 1){
+                sql.append(", \n");
             }
         }
 
         sql.append(")");
         sql.append(sinkExtend);
 
-        String createTableSql = sql.toString();
-        System.out.println(createTableSql);
+        String createSQL = sql.toString();
+
         PreparedStatement preparedStatement = null;
+
         try {
-            preparedStatement = connection.prepareStatement(createTableSql);
+            preparedStatement = connection.prepareStatement(createSQL);
             preparedStatement.execute();
         }catch (SQLException sqlException){
             sqlException.printStackTrace();
-            System.out.println("建表语句：-- > " + createTableSql + "执行异常");
+            throw new RuntimeException("建表语句\n" + createSQL + "\n执行时出现了异常");
         }finally {
             if (preparedStatement != null){
                 try {
                     preparedStatement.close();
                 }catch (SQLException sqlException){
                     sqlException.printStackTrace();
-                    throw  new RuntimeException("数据库操作对象释放异常");
+                    throw new RuntimeException("数据库操作对象释放异常");
                 }
             }
         }
