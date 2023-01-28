a23c586a2176eeef1ba1e84eb274d7fd4815a4b6
mmdxhg <mmdxhg@163.com>
2023-01-28 19:14:36 +0800

    time: 2022_01_18 info:first commit for flinkReLearn work: broadcast Stream and maxwell_mysqlDS for dim

---
+++ src/main/java/com/atguigu/gmall/realtime/util/MySqlUtil.java
@@ -0,0 +1,31 @@
+package com.atguigu.gmall.realtime.util;
+
+public class MySqlUtil {
+
+
+    public static String getBaseDicLookUpDDL(){
+        return "create table `base_dic`(\n" +
+                "dic_code string,\n" +
+                "dic_name string,\n" +
+                "parent_code string,\n" +
+                "create_time timestamp,\n" +
+                "operate_time timestamp,\n" +
+                "primary key(`dic_code`) not enforced\n" +
+                ") " + MySqlUtil.mysqlLookUpTableDDL("base_dic");
+    }
+
+    private static String mysqlLookUpTableDDL(String tableName) {
+
+        String ddl = "WITH (\n" +
+                "'connector' = 'jdbc',\n" +
+                "'url' = 'jdbc:mysql://hadoop102:3306/gmall',\n" +
+                "'table-name' = '" + tableName + "',\n" +
+                "'lookup.cache.max-rows' = '10',\n" +
+                "'lookup.cache.ttl' = '1 hour',\n" +
+                "'username' = 'root',\n" +
+                "'password' = '000000',\n" +
+                "'driver' = 'com.mysql.cj.jdbc.Driver'\n" +
+                ")";
+        return ddl;
+    }
+}
